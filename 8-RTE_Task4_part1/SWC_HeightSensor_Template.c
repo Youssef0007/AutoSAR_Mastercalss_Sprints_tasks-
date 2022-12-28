cfa80b2b186b6fe8fbc72a1dd2650b2f64437975
Youssef Abdelaziz <youssefabdelaziz1581998@gmail.com>
2022-12-28 23:03:20 +0200

    2nd commit

---
+++ 8-RTE_Task4_part1/SWC_HeightSensor_Template.c
@@ -0,0 +1,30 @@
+/**
+ *
+ * \file SWC_HeightSensor_Template.c
+ * \brief Rte Component Template for AUTOSAR SWC: SWC_HeightSensor
+ *
+ * \author Sprints AUTOSAR Authoring Tool (SAAT) v1.0.2
+ * Generated on 12/28/2022 10:29 PM
+ *
+ * For any inquiries: hassan.m.farahat@gmail.com
+ *
+ */
+
+#include "Rte_SWC_HeightSensor.h"
+
+
+/**
+ *
+ * Runnable RE_HeightSensor_GetPosition
+ *
+ * Triggered By:
+ *  - OperationInvokedEventImpl.OIE_ppHeightSensor_Opr_GetPostion
+ *
+ */
+
+void RE_HeightSensor_GetPosition (Impl_SensorPostionType* Arg_position)
+{
+	Std_ReturnType status;
+
+}
+
---
+++ 8-RTE_Task4_part1/SWC_SlideSensor_Template.c
@@ -0,0 +1,30 @@
+/**
+ *
+ * \file SWC_SlideSensor_Template.c
+ * \brief Rte Component Template for AUTOSAR SWC: SWC_SlideSensor
+ *
+ * \author Sprints AUTOSAR Authoring Tool (SAAT) v1.0.2
+ * Generated on 12/28/2022 10:30 PM
+ *
+ * For any inquiries: hassan.m.farahat@gmail.com
+ *
+ */
+
+#include "Rte_SWC_SlideSensor.h"
+
+
+/**
+ *
+ * Runnable RE_SlideSensor_GetPosition
+ *
+ * Triggered By:
+ *  - OperationInvokedEventImpl.OIE_ppSlideSensor_Opr_GetPostion
+ *
+ */
+
+void RE_SlideSensor_GetPosition (Impl_SensorPostionType* Arg_position)
+{
+	Std_ReturnType status;
+
+}
+
